7e8977096210061884360d46468d41d683f01d42
Matt VanZoest <matthew.vanzoest@gmail.com>
2021-12-17 16:47:21 -0700

    euclidean algorithm to compute gcd

---
+++ AlgorithmPractice/Algorithms/EuclideanAlgorithm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AlgorithmPractice.Algorithms
+{
+    public static class EuclideanAlgorithm
+    {
+        public static int ComputeGcd(int a, int b)
+        {
+            a.ThrowIfNonPositive();
+            b.ThrowIfNonPositive();
+
+            var big = Math.Max(a, b);
+            var small = Math.Min(a, b);
+
+            var remainder = big % small;
+
+            while (remainder != 0)
+            {
+                big = small;
+                small = remainder;
+                remainder = big % small;
+            }
+
+            return small;
+        }
+
+        private static void ThrowIfNonPositive(this int i)
+        {
+            if (i < 1)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ AlgorithmPracticeTests/EuclideanAlgorithmTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using AlgorithmPractice.Algorithms;
+using FluentAssertions;
+using Xunit;
+
+namespace AlgorithmPracticeTests
+{
+    public class EuclideanAlgorithmTests
+    {
+        [Fact]
+        public void ComputeGcd_When1071And462_Returns21()
+        {
+            EuclideanAlgorithm.ComputeGcd(1071, 462).Should().Be(21);
+        }
+
+        [Fact]
+        public void ComputeGcd_When1And1_Returns1()
+        {
+            EuclideanAlgorithm.ComputeGcd(1, 1).Should().Be(1);
+        }
+
+        [Fact]
+        public void ComputeGcd_When5And1_Returns1()
+        {
+            EuclideanAlgorithm.ComputeGcd(5, 1).Should().Be(1);
+        }
+
+        [Fact]
+        public void ComputeGcd_When3And6_Returns3()
+        {
+            EuclideanAlgorithm.ComputeGcd(3, 6).Should().Be(3);
+        }
+
+        [Theory]
+        [InlineData(0, 1)]
+        [InlineData(1, 0)]
+        [InlineData(-1, -1)]
+        public void ComputeGcd_WhenInvalidInput_Throws(int n1, int n2)
+        {
+            Assert.Throws<InvalidOperationException>(() => EuclideanAlgorithm.ComputeGcd(n1, n2));
+        }
+    }
+}
