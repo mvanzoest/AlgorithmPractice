a010b0ce257f3c00476beea436840425e898dae9
Matt VanZoest <matthew.vanzoest@gmail.com>
2021-12-17 15:37:18 -0700

    multiplication

--- AlgorithmPractice/Algorithms/MultiplicationAlgorithm.cs
+++ AlgorithmPractice/Algorithms/MultiplicationAlgorithm.cs
@@ -1,25 +1,50 @@
-﻿namespace AlgorithmPractice.Algorithms
+﻿using System.Collections.Generic;
+using System.Text;
+using AlgorithmPractice.Utilities;
+
+namespace AlgorithmPractice.Algorithms
 {
     public static class MultiplicationAlgorithm
     {
         public static string Multiply(string factor1, string factor2)
         {
+            var subProducts = new List<string>();
+
             for (var i = factor2.Length - 1; i >= 0; i--)
             {
+                var start = new string('0', factor2.Length - (i + 1));
+                var subProduct = new StringBuilder(start);
+                var carry = 0;
+
                 for (var j = factor1.Length - 1; j >= 0; j--)
                 {
-                    var digit1 = factor1[j];
-                    var digit2 = factor2[i];
+                    var digit1 = factor1[j].ToDigit();
+                    var digit2 = factor2[i].ToDigit();
 
+                    var innerProduct = digit1 * digit2 + carry;
+                    var result = innerProduct % 10;
 
+                    carry = innerProduct / 10;
+
+                    subProduct.Insert(0, result.ToString());
                 }
+
+                if (carry > 0)
+                {
+                    subProduct.Insert(0, carry.ToString());
+                }
+
+                subProducts.Add(subProduct.ToString());
             }
-            var d1 = int.Parse(factor1);
-            var d2 = int.Parse(factor2);
 
-            var product = d1 * d2;
+            var product = "0";
 
-            return product.ToString();
+            foreach (var subProduct in subProducts)
+            {
+                product = AdditionAlgorithm.Add(product, subProduct);
+            }
+
+            return product;
         }
     }
 }
