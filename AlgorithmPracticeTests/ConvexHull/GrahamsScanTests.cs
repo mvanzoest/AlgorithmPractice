f55b6b41660b5fdcb04771360e3f8a1085ba68a9
Matt VanZoest <matthew.vanzoest@gmail.com>
2021-12-18 16:34:08 -0700

    wow it worked

--- AlgorithmPracticeTests/ConvexHull/GrahamsScanTests.cs
+++ AlgorithmPracticeTests/ConvexHull/GrahamsScanTests.cs
@@ -35,5 +35,44 @@ namespace AlgorithmPracticeTests.ConvexHull
                 result.Should<Point>().Contain(plane[i]);
             }
         }
+
+        [Fact]
+        public void Scan_Chapter1Example_ComputesCorrectly()
+        {
+            // Arrange
+            var plane = new[]
+            {
+                new Point(0, 0), // 0 - hull
+                new Point(1, 10), // 1 - hull
+                new Point(2, 4), // 2
+                new Point(4, 14), // 3 - hull
+                new Point(7, 11), // 4
+                new Point(11, 16), // 5 - hull
+                new Point(6, -1), // 6
+                new Point(14, 12), // 7
+                new Point(12, 6), // 8
+                new Point(20, 12.25), // 9 - hull
+                new Point(18, 3), // 10
+                new Point(15.5, -1.25), // 11
+                new Point(23, -5), // 12 - hull
+                new Point(11.5, -8), // 13 - hull
+                new Point(3, -10), // 14 - hull
+            };
+
+            // Act
+            var result = GrahamsScan.Scan(plane);
+
+            // Assert
+            result.Should<Point>().HaveCount(8);
+
+            result.Should<Point>().Contain(plane[0]);
+            result.Should<Point>().Contain(plane[1]);
+            result.Should<Point>().Contain(plane[3]);
+            result.Should<Point>().Contain(plane[5]);
+            result.Should<Point>().Contain(plane[9]);
+            result.Should<Point>().Contain(plane[12]);
+            result.Should<Point>().Contain(plane[13]);
+            result.Should<Point>().Contain(plane[14]);
+        }
     }
 }
