b37a1eef3fe436a34a6cc61cce7f689893225e58
Matt VanZoest <matthew.vanzoest@gmail.com>
2021-12-18 19:04:47 -0700

    dynamic programming to find max integer

---
+++ AlgorithmPractice/Algorithms/Math/DynamicMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmPractice.Algorithms.Math
+{
+    public static class DynamicMax
+    {
+        /// <summary>
+        /// Finds the max in numbers using Dynamic Programming.
+        /// </summary>
+        public static int Max(List<int> numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException(nameof(numbers));
+            }
+
+            if (numbers.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (numbers.Count == 1)
+            {
+                return numbers[0];
+            }
+
+            var count = numbers.Count / 2;
+
+            var max1 = Max(numbers.GetRange(0, count));
+            var max2 = Max(numbers.GetRange(count, numbers.Count - count));
+
+            return max1 > max2 ? max1 : max2;
+        }
+    }
+}
