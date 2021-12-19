432ab9cc0d886d2537faa0fe752585f6a94e3557
Matt VanZoest <matthew.vanzoest@gmail.com>
2021-12-18 22:37:11 -0700

    some refactoring

--- AlgorithmPractice/Algorithms/String/MinimumEditDistance.cs
+++ AlgorithmPractice/Algorithms/String/MinimumEditDistance.cs
@@ -6,45 +6,52 @@
         {
             var matrix = new int[s1.Length + 1, s2.Length + 1];
 
-            for (var i = 0; i <= s1.Length; i++)
+            Initialize(matrix, s1, s2);
+
+            for (var i = 1; i <= s1.Length; i++)
             {
-                for (var j = 0; j <= s2.Length; j++)
+                for (var j = 1; j <= s2.Length; j++)
                 {
-                    if (j == 0)
-                    {
-                        matrix[i, j] = i;
-                    }
-                    else if (i == 0)
-                    {
-                        matrix[i, j] = j;
-                    }
-                    else
-                    {
-                        var cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
+                    var cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
 
-                        var costs = new[]
-                        {
-                            matrix[i - 1, j - 1] + cost, // replace
-                            matrix[i - 1, j] + 1, // remove
-                            matrix[i, j - 1] + 1, // insert
-                        };
+                    var replace = matrix[i - 1, j - 1] + cost;
+                    var remove = matrix[i - 1, j] + 1;
+                    var insert = matrix[i, j - 1] + 1;
 
-                        var minimum = int.MaxValue;
+                    var minimum = Min(replace, remove, insert);
 
-                        foreach (var c in costs)
-                        {
-                            if (c < minimum)
-                            {
-                                minimum = c;
-                            }
-                        }
-
-                        matrix[i, j] = minimum;
-                    }
+                    matrix[i, j] = minimum;
                 }
             }
 
             return matrix[s1.Length, s2.Length];
+        }
+
+        private static void Initialize(int[,] matrix, string s1, string s2)
+        {
+            for (var i = 0; i <= s1.Length; i++)
+            {
+                matrix[i, 0] = i;
+            }
+            for (var j = 0; j <= s2.Length; j++)
+            {
+                matrix[0, j] = j;
+            }
+        }
+
+        private static int Min(params int[] values)
+        {
+            var minimum = int.MaxValue;
+
+            foreach (var value in values)
+            {
+                if (value < minimum)
+                {
+                    minimum = value;
+                }
+            }
+
+            return minimum;
         }
     }
 }
